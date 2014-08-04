993129d4e9a2f74ebfb954f5e57df59c59a5303f
David Haines <dlhaines@umich.edu>
2014-08-04 09:57:55 -0400

    add logging

--- courselist.rb
+++ courselist.rb
@@ -4,10 +4,25 @@ require 'helpers/DataHelper'
 
 class CourseList < Sinatra::Base
 
+  ## make sure logging is available
+  configure :production, :development do
+                         enable :logging
+                         log = File.new("log/sinatra.log", "a+") 
+                         $stdout.reopen(log)
+                         $stderr.reopen(log)
+
+                         $stderr.sync = true
+                         $stdout.sync = true
+                       end
+
   helpers DataHelper
 
-# Indent html for pretty debugging and do not sort attributes (Ruby 1.9)
-Slim::Engine.set_default_options pretty: true, sort_attrs: false
+  # Indent html for pretty debugging and do not sort attributes (Ruby 1.9)
+
+  # pretty print html for development
+  Slim::Engine.set_default_options pretty: true, sort_attrs: false
+
+
 
   get '/' do
     slim :home
@@ -22,6 +37,7 @@ Slim::Engine.set_default_options pretty: true, sort_attrs: false
 
   get '/courses/:userid' do |user|
     ## call helper with parameters and get back locals array to pass to slim template engine.
+    logger.info "courses/:userid: #{user}"
     courseDataForX = CourseData(user)
     slim :courses, locals: {userid: user, courseData: courseDataForX}
   end
