e495ade66b23dfae083e7fe25e9f8df612b0b273
David Haines <dlhaines@umich.edu>
2014-09-12 15:56:23 -0400

    add file based data provider

--- server/courselist.rb
+++ server/courselist.rb
@@ -48,6 +48,13 @@ END
 
   ### helper function
   def CourseData(a)
+    #return CourseDataStatic(a)
+    puts "CourseData a: #{a}"
+    return CourseDataFile(a)
+  end
+
+  def CourseDataStatic(a)
+    puts "data provider is CourseDataStatic\n"
     classJson = 
       [
         { :title => "English 323",
@@ -72,27 +79,42 @@ END
           :instructor_email => "google@google.goo"
         }
       ]
-    
     return classJson
   end
 
- @@FAKE_UNIQNAME = ["ME","YOU","THEY","instx","dlhaines","csev"]
+  @@FAKE_UNIQNAME = ["ME","YOU","THEY","instx","dlhaines","csev"]
+
+  def CourseDataFile(a)
+    puts "data provider is CourseDataFile\n"
+    dataFile = @@ls['data_file_dir']+"/"+@@ls['data_file_type']+"/#{a}.json"
+    puts "data file string: "+dataFile
+
+    if File.exists?(dataFile) 
+      logger.debug("file exists: #{dataFile}")
+      classes = File.read(dataFile)
+    else 
+      logger.debug("file does not exist: #{dataFile}")
+      classes = "404"
+    end
+
+    return classes
+  end
 
   ########### ROUTERS ##############
 
   ### TESTING:
-#### for the moment generate a fake uniqname in request.env['REMOTE_USER'] 
-#### for each request so can checkout what happens when different uniqnames are used.
-#### This entire "before" section can be deleted when this testing is done as it
-#### works by resetting the variable the real code will use.
+  #### for the moment generate a fake uniqname in request.env['REMOTE_USER'] 
+  #### for each request so can checkout what happens when different uniqnames are used.
+  #### This entire "before" section can be deleted when this testing is done as it
+  #### works by resetting the variable the real code will use.
   before do
-   offset = Random.rand(@@FAKE_UNIQNAME.length)
+    offset = Random.rand(@@FAKE_UNIQNAME.length)
     fake_uname = @@FAKE_UNIQNAME[offset]
     puts "fake_uname: #{fake_uname} offset: #{offset}"
     request.env['REMOTE_USER'] = fake_uname
   end
 
-  ## by default invoke the UI.
+  ## If they don't say something different invoke the UI
 
   ### return the index.html page but replace the value of UNIQNAME by
   ### the contents of the request remote user parameter.
@@ -108,7 +130,7 @@ END
   get '/api' do
     @@apidoc
     ## inline may require classic, not modular, organization
- #    slim :apidocA
+    #    slim :apidocA
   end
 
   ## dump settings to log upon request`
@@ -118,14 +140,22 @@ END
   end
 
   ### return json array of the course objects for this user.  Not specifying 
-  ### json as format is an error
+  ### json as format is an error.
   get '/courses/:userid.?:format?' do |user, format|
     logger.info "courses/:userid: #{user} format: #{format}"
     if format && "json".casecmp(format).zero? 
       content_type :json
       courseDataForX = CourseData(user)
+      logger.info "courseDataForX: #{courseDataForX}"
+      if "404".casecmp(courseDataForX).zero?
+        response.status = 404
+        return ""
+      end
+      # parse return value as json to convert from string
+      courseDataForXJson = JSON.parse courseDataForX
       logger.info "courseData #{courseDataForX}"
-      courseDataForX.to_json
+      # return json
+      courseDataForXJson.to_json
     else
       response.status = 400
       return "format missing or not supported: [#{format}]"
@@ -146,7 +176,7 @@ __END__
 
 ### API documentation as slim template.
 @@ apidocA
- h1 This is the API documentation
+h1 This is the API documentation
   h2 '/api' will return api documentation.
   h2 '/courses/{userid}.json' will return course objects for this user.
 
