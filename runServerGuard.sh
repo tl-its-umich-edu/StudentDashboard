6ac0a2a9ab180992646dc81f940bb24caa1d04e6
David Haines <dlhaines@umich.edu>
2014-10-03 15:00:25 -0400

    TLPORTAL-54: fix anon user handing and make launch consistent between local and tomcat environments

--- runServerGuard.sh
+++ runServerGuard.sh
@@ -1,7 +1,13 @@
- #!/bin/bash
+#!/bin/bash
 set -x
-(cd ./server;
-    ./runServerGuard.sh;
-)
-#end
+## If there is a xterm use it.
+if [ -e /opt/X11/bin/xterm ]; then
+    /opt/X11/bin/xterm -e bundle exec guard &
+    sleep 5;
+    /opt/X11/bin/xterm -e tail -F server/log/sinatra.log &
+else
+    # otherwise just run guard in current terminal.
+    bundle exec guard &
+fi
 
+#end
