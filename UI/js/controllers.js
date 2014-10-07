780b45841667c538b7f29e6d8aded6cc7eda2038
Gonzalo Silverio <gsilver@0587385433.wireless.umich.net>
2014-10-07 15:07:56 -0400

    renderer now works with ESB data format - except it assumes instructors are an array

--- UI/js/controllers.js
+++ UI/js/controllers.js
@@ -12,7 +12,7 @@ dashboardApp.controller('scheduleController', ['$scope', '$http', function ($sco
 dashboardApp.controller('coursesController', ['$scope', '$http', function ($scope, $http) {
   var url = 'courses/' + userId + '.json';
   $http.get(url).success(function (data) {
-    $scope.courses = data;
+    $scope.courses = data.getMyRegClassesResponse.RegisteredClasses;
   });
 }]);
 
