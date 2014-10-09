541ecea379cb2214dd0e2374ff2caef757325ce3
David Haines <dlhaines@umich.edu>
2014-10-09 09:04:39 -0400

    Add build shell script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Build script to assemble and test StudentDashboard.
+
+## Setup RVM
+source ~/.rvm/scripts/rvm
+# verify that it is setup
+type rvm | head -n 1
+# select and setup a particular ruby version.
+rvm use ruby-1.9.3-p484
+#rvm list
+
+set -x
+
+## should test return code
+
+warble
+
+#end
