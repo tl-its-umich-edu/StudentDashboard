acd33e0aca081990b33cfd5c77d90e923c971000
David Haines <dlhaines@umich.edu>
2014-10-24 12:30:15 -0400

    Check for running server before running new server

--- runServerBundle.sh
+++ runServerBundle.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 ## run server that doesn't check for changes but reports 
 ## startup errors far better.
+
+PORT=3000
+
+## get rid of logs
 if [ -e ./server/log/sinatra.log ]; then
     rm ./server/log/sinatra.log
 fi
-bundle exec rackup -p 3000
+
+## check for a running server on this port
+LINES=$(lsof -i TCP:$PORT | wc -l)
+
+if [ $LINES != 0 ]; then
+    echo "exiting: found previous server on port $PORT"
+    echo $(lsof -i TCP:$PORT)
+    exit 1;
+fi
+
+bundle exec rackup -p $PORT
 #end
