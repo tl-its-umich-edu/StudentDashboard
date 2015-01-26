a5ef4a285450182d8559ade23b3279a223eaae4e
David Haines <dlhaines@umich.edu>
2015-01-26 16:34:51 -0500

    TLPORTAL-135: make list of admin users depend on MCommunity group

---
+++ server/spec/testLdap.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -x
+DN="dc=umich,dc=edu?uid=dlhainesXXX"
+#H="ldap://ldap.itd.umich.edu:389/dc=umich,dc=edu"
+H="ldap://ldap.itd.umich.edu:389"
+#ldapwhoami -v -x -H $H -D $DN
+#ldapwhoami -vvv -h <hostname> -p <port>  -D <binddn> -x -w <passwd> where binddn is the DN of the person whose credentials you are authenticating
+#ldapwhoami -x -D "cn=Manager,dc=example,dc=com" -W
+
+#ldap://ldap.itd.umich.edu/ou=People,dc=umich,dc=edu?title,postalAddress,telephoneNumber,facsimileTelephoneNumber,mail,onVacation,vacationMessage,drink?sub?uid=pturgyan
+
+#ldapsearch -x -h medusa.lngs.itd.umich.edu -p 389 -b '' -s sub -D"cn=slurpduser,o=services" -W uid=pturgyan
+
+##ldapsearch -H ldaps://ldap-dev.itd.umich.edu:4444 -D"uid=xpaul,ou=people,dc=umich,dc=edu" -W -x -LLL uid=xpaul mailforwardingaddress
+# this seems to work but only gives single dn: entry.
+#ldapsearch -H ldap://ldap.itd.umich.edu:389 -D"uid=dlhaines,ou=people,dc=umich,dc=edu" -L uid=dlhaines mailforwardingaddress
+### This does work :-):-)
+#ldapsearch -H ldap://ldap.itd.umich.edu:389 -D"uid=dlhaines,ou=people,dc=umich,dc=edu" -L uid=dlhaines
+## try to get group
+#F='(&(cn=ctsupportstaff) (objectclass=rcf822MailGroup))'
+######
+## The following works to get the members
+#F=(cn=ctsupportstaff)
+#ldapsearch -H ldap://ldap.itd.umich.edu:389 -D"ou=Groups,dc=umich,dc=edu" -L $F
+######
+
+### works to get only members
+F='(&(cn=ctsupportstaff)(objectclass=rfc822MailGroup))'
+ldapsearch -H ldap://ldap.itd.umich.edu:389 -D"ou=Groups,dc=umich,dc=edu" -L $F member
+
+
+# does not work to find membership
+#ldapsearch -v  -H ldap://ldap.itd.umich.edu:389 -D"ou=Groups,dc=umich,dc=edu,uid=dlhaines" -L $F member
+
+## suggested to find membership:
+#ldapsearch -x -D "ldap_user" -w "user_passwd" -b "cn=jdoe,dc=example,dc=local" -h ldap_host '(memberof=cn=officegroup,dc=example,dc=local)'
+
+
+
+# this gets "administrativei limit exceeded"
+#ldapsearch -H ldap://ldap.itd.umich.edu:389 -D"uid=dlhaines,ou=people,dc=umich,dc=edu" -L
+
+#### http://ds.med.umich.edu/idmt/dev_ldap_authentication.html
+## group search example from med
+#filter: "(&(objectClass=Group)(|(cn=mcit-iso*)(cn=umhs*))(member=cn=darthvader,ou=people,dc=med,dc=umich,dc=edu))"
+
+#### sample
+#Enter LDAP Password:
+#dn: uid=xpaul,ou=People,dc=umich,dc=edu
+#mailForwardingAddress: pturgyan@quince.ifs.umich.edu
+#mailForwardingAddress: xpaul@med.umich.edu
+#ldapsearch -H ldaps://ldap-dev.itd.umich.edu:4567 -x -W -LLL -D"uid=xpaul,ou=People,dc=umich,dc=edu" uid=xpaul mailforwardingaddress
+#Enter LDAP Password:
+#dn: uid=xpaul,ou=People,dc=umich,dc=edu
+#mailForwardingAddress: xpaul@da.dir.3456
--- server/spec/test_ldap_check.rb
+++ server/spec/test_ldap_check.rb
@@ -1,14 +1,11 @@
 # Integration test to verify that a user can be found in
 # an MCommunity group if they are present.
-
 require_relative 'test_helper'
 require 'rubygems'
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/unit'
-#require 'rest-client'
 require 'logger'
-#require 'yaml'
 require_relative '../../server/Logging'
 
 ## set the environment for testing
@@ -17,6 +14,7 @@ require_relative '../../server/Logging'
 require 'net-ldap'
 
 require_relative '../ldap_check'
+
 
 class LdapTest < MiniTest::Test
   include Logging
@@ -70,5 +68,143 @@ class LdapTest < MiniTest::Test
     found = @x.is_user_in_admin_hash "GODZILLA_XXX"
     refute found,"checking GOZILLA_XXX in group"
   end
+
+  # def test_memberFilter
+  #   m = LdapCheck.new({"filter_prefix" => "FIRST", "filter_suffix" => "LAST"})
+  #   f = @x.memberFilter("them")
+  #   assert_equal("(&(cn=them) (objectclass=rcf822MailGroup))",f,"build correct member filter")
+  # end
+
+  #def test_yml
+#
+#  end
+
+  # # GET THE DISPLAY NAME AND E-MAIL ADDRESS FOR A SINGLE USER
+  # search_param = "lstarr"
+  # result_attrs = ["sAMAccountName", "displayName", "mail"]
+  #
+  # # Build filter
+  # search_filter = Net::LDAP::Filter.eq("sAMAccountName", search_param)
+  #
+  # # Execute search
+  # ldap.search(:filter => search_filter, :attributes => result_attrs, :return_result => false) { |item|
+  #   puts "#{item.sAMAccountName.first}: #{item.displayName.first} (#{item.mail.first})"
+  # }
+  #
+
+  #F='(&(cn=ctsupportstaff)(objectclass=rfc822MailGroup))'
+  #ldapsearch -H ldap://ldap.itd.umich.edu:389 -D"ou=Groups,dc=umich,dc=edu" -L $F member
+  # filter: '(&(member=uid=dlhaines,ou=People,dc=umich,dc=edu)(cn=its tl staff))'
+
+  def find_user_in_ldap_members user, members
+    puts "user: #{user} members: #{members}"
+    found = false
+    members.each { |e| found = true if e.start_with? "uid=#{user}," }
+    found
+  end
+
+  def find_user_in_ldap_members_any user, members
+    members.any? { |e| e.start_with? "uid=#{user}," }
+  end
+
+  def test_raw()
+    puts "test_raw"
+    Net::LDAP.open(:host => "ldap.itd.umich.edu",
+                   :port => 389,
+                   :base => "ou=Groups,dc=umich,dc=edu") do |ldap|
+      # Do all your LDAP stuff here...
+      p ldap
+
+      #fstring = '(&(cn=ctsupportstaff)(objectclass=rfc822MailGroup))'
+      #group = "its tl staff"
+      group = "ctsupportstaff"
+      user = "dlhaines.XXX"
+      #fstring = '(&(member=uid=dlhaines,ou=People,dc=umich,dc=edu)(cn=its tl staff))' # works to get all
+      #fstring = "(&(member=uid=dlhaines,ou=People,dc=umich,dc=edu)(cn=#{group}))" # works to get all
+      #fstring = "(&(member=uid=#{user},ou=People,dc=umich,dc=edu)(cn=#{group}))" # works to get all
+
+      #fstring = "(cn=#{group})" # works to get all
+
+      fstring = "(&(cn=#{group})(objectclass=rfc822MailGroup))"
+
+      f = Net::LDAP::Filter.construct(fstring)
+      puts "filter: f: "
+      p f
+      x = ldap.search(:filter=>f)
+      puts "search result x:"
+      p x.each {|a| puts a}
+
+      puts "dn:"
+      p x[0].dn
+      found = false
+      puts "members"
+      x[0].member.each   { |e| puts e}
+      x[0].member.each   { |e| found = true if e.start_with? "uid=#{user}," }
+      #ldap.search(...)
+
+      puts "found: #{found}"
+
+      find_by_method = find_user_in_ldap_members_any "dlhaines.XXX", x[0].member
+      puts "fuilm: ", find_by_method
+    end
+  end
+
+  #### NOTE: this method can veto requests, so the "assert r" is checking that
+  # #### that the request is vetoed and "refute r" checks that the request is
+  # #### not vetoed.
+  # #### NOTE: The messages describe the failure state of the test.
+  #
+  # ### test that have a user to check against.
+  # def test_nil_user_fails
+  #   r = CourseList.vetoRequest nil, @coursesUrlststvii
+  #   assert r, "veto if no authenticated user (nil)"
+  # end
+  #
+  # def test_empty_user_fails
+  #   r = CourseList.vetoRequest "", @coursesUrlststvii
+  #   assert r, "veto if no authenticated user (empty string)"
+  # end
+  #
+  #
+  # ### check to make sure unauthorized user can not request courses but
+  # ### and anothorized user can
+  # def test_mismatched_user
+  #   r = CourseList.vetoRequest "abba", @coursesUrlststvii
+  #   assert r, "allowed wrong user."
+  # end
+  #
+  # ## make sure that the user matches
+  # def test_matched_user
+  #   r = CourseList.vetoRequest "ststvii", @coursesUrlststvii
+  #   refute r, "refused correct user"
+  # end
+  #
+  # ### Check that administrative users can do anything
+  #
+  # #def test_admin_user_works
+  # #  skip("user from admin list should not be vetoed")
+  # #end
+  #
+  # ####### check that don't affect irrelevant URLS
+  #
+  # def test_topUrl
+  #   r = CourseList.vetoRequest "ststvii", @topUrl
+  #   refute r, "refused correct url"
+  # end
+  #
+  # def test_topSDUrl
+  #   r = CourseList.vetoRequest "ststvii", @topSDUrl
+  #   refute r, "refused correct url"
+  # end
+  #
+  # def test_topSDUrlUNIQNAME_different
+  #   r = CourseList.vetoRequest "ststvii", @topSDUrlUNIQNAME_different
+  #   refute r, "refused correct url (url does not have /courses/)"
+  # end
+  #
+  # def test_topSDUrlUNIQNAME_same
+  #   r = CourseList.vetoRequest "ststvii", @topSDUrlUNIQNAME_same
+  #   refute r, "refused correct url"
+  # end
 
 end
