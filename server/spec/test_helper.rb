f88c36dc56209b43ffc2ab7a0ceb15bbab0e69e6
David Haines <dlhaines@umich.edu>
2015-08-12 10:34:00 -0400

    TLPORTAL-271: update testing too

--- server/spec/test_helper.rb
+++ server/spec/test_helper.rb
@@ -1,4 +1,9 @@
 require 'simplecov'
+
+## The SD Logger class must be required before including this TestHelper class.
+## It should NOT be included directly in a test class itself.  It should be included
+## by including the class under test.
+
 SimpleCov.start do
   filters.clear
   add_filter "/.rvm/"
@@ -42,4 +47,50 @@ SimpleCov.start do
 
 
   end
+end
+
+include Logging
+
+class TestHelper < Object
+
+  # store a global default log level that test files can
+  # look up and share.
+  @@log_level = Logger::Severity::WARN
+  #@@log_level  = Logger::Severity::DEBUG
+
+  def self.getCommonLogLevel
+    return @@log_level
+  end
+
+  ## return a fully qualified path to the test file directory
+  ## It assumes standard locations and looking for directory with name 'test-files'
+  def self.findTestFileDirectory
+    currentDirectory = Dir.pwd()
+
+    checkDir = "#{currentDirectory}/server/test-files"
+    return checkDir if Dir.exist?(checkDir);
+
+    checkDir = File.dirname(currentDirectory)+"/test-files"
+    return checkDir if Dir.exist?(checkDir);
+
+    # didn't find anything.
+    puts "CAN NOT FIND TEST FILE DIRECTORY: ";
+    exit 1
+  end
+
+  ## return a fully qualified path to the directory with the security file.
+  ## It assumes standard locations and looking for directory with name 'test-files'
+  def self.findSecurityFile(file_name)
+    currentDirectory = Dir.pwd()
+
+    checkDir = "#{currentDirectory}/#{file_name}"
+    return checkDir if File.exist?(checkDir);
+
+    checkDir = currentDirectory+"/server/spec/#{file_name}"
+    return checkDir if File.exist?(checkDir);
+
+    puts "CAN NOT FIND SECURITY FILE: #{file_name}";
+    exit 1
+  end
+
 end
