9efd6a781e5f68ed20d1760a6ac047d5696674bc
David Haines <dlhaines@umich.edu>
2015-08-14 12:42:35 -0400

    TLPORTAL-271, fix missing merge fix

--- server/spec/test_helper.rb
+++ server/spec/test_helper.rb
@@ -47,11 +47,11 @@ class TestHelper < Object
   def self.findSecurityFile(file_name)
     currentDirectory = Dir.pwd()
 
-    checkDir = "#{currentDirectory}/#{file_name}"
-    return checkDir if File.exist?(checkDir);
+    checkFile = "#{currentDirectory}/#{file_name}"
+    return checkFile if File.exist?(checkFile);
 
-    checkDir = currentDirectory+"/server/spec/#{file_name}"
-    return checkDir if File.exist?(checkDir);
+    checkFile = currentDirectory+"/server/spec/#{file_name}"
+    return checkFile if File.exist?(checkFile);
 
     puts "CAN NOT FIND SECURITY FILE: #{file_name}";
     exit 1
