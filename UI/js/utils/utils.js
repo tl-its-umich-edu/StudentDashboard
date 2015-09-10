a53813f900caf34edadf126a109332f1ad0824e3
Gonzalo Silverio <gsilver@umich.edu>
2015-09-10 12:48:06 -0400

    TLPORTAL-293 - fade background images to #444 and remove unneeded images

--- UI/js/utils/utils.js
+++ UI/js/utils/utils.js
@@ -100,10 +100,12 @@ $(document).ready(function(){
 
       //need to get absolute path to deal with Chrome/Safari
       document.body.style.backgroundImage = 'url(' + window.location + 'external/image/' + ramdomImage + ')';
+      document.body.style.backgroundColor = '#444444';
     })
     .fail(function() {
       // select a default image and assign it to the body element
       document.body.style.backgroundImage = 'url(' + window.location + 'data/images/back/default.jpg' + ')';
+      document.body.style.backgroundColor = '#444444';
     });
   }
 });
