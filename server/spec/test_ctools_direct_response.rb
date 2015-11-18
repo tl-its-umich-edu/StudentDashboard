1b111fe694befdfcf7f0e22a073aa0affdb5086d
David Haines <dlhaines@umich.edu>
2015-11-18 12:13:23 -0500

    TLPORTAL-358: filter ctools dash feed

--- server/spec/test_ctools_direct_response.rb
+++ server/spec/test_ctools_direct_response.rb
@@ -32,13 +32,13 @@ class TestCtoolsDirectResponse < Minitest::Test
 
   # verify that processing the minimal string version works
   def test_new_creates_something
-    @response = CToolsDirectResponse.new(@@string_A)
+    @response = CToolsDirectResponse.new(@@string_A, Hash.new)
     refute_nil @response, "get connector object"
     refute_nil @@testFileDir, "locate test file directory"
   end
 
   def test_string_A_json_todolms
-    response = CToolsDirectResponse.new(@@string_A)
+    response = CToolsDirectResponse.new(@@string_A, Hash.new)
     tdl = response.toDoLms
     assert_equal 0, tdl.length, "verify length of empty response"
   end
@@ -64,13 +64,100 @@ class TestCtoolsDirectResponse < Minitest::Test
     file_as_json = JSON.parse(file_as_string)
     file_data = file_as_json['Result']
     file_data_as_string = JSON.generate(file_data)
-    response = CToolsDirectResponse.new(file_data_as_string)
+    response = CToolsDirectResponse.new(file_data_as_string, Hash.new)
     logger.debug "#{__method__}: #{__LINE__}: response: "+response.inspect
     dash_format = response.toDoLms
 
-    assert_equal 3, dash_format.length, 'got multiple assignments'
-    assert_equal 10, dash_format[0][:due_date_sort].length
     logger.debug "#{__method__}: #{__LINE__}: dash_format: "+dash_format.inspect
+    assert_equal 2, dash_format.length, 'got multiple assignments, one filtered out'
+    assert_equal 10, dash_format[0][:due_date_sort].length
+
+  end
+
+
+  def test_get_ctools_collection_multiple_keys_todolms
+
+    # Test with sample of multiple assignments.
+    # The 'ctoolsXX' files are full results, not unit test data, so:
+    # read it in, strip off the WAPI wrapper, stringify it and then process.
+
+    file_name = "assignment_multiple_keys"
+    file_as_string = IO.read("#{@@testFileDir}/todolms/ctools/#{file_name}.json")
+
+    file_as_json = JSON.parse(file_as_string)
+    file_data = file_as_json['Result']
+    file_data_as_string = JSON.generate(file_data)
+    response = CToolsDirectResponse.new(file_data_as_string, Hash.new)
+    logger.debug "#{__method__}: #{__LINE__}: response: "+response.inspect
+    dash_format = response.toDoLms
+
+    logger.debug "#{__method__}: #{__LINE__}: dash_format: "+dash_format.inspect
+    assert_equal 13, dash_format.length, 'got multiple assignments, one filtered out'
+    assert_equal 10, dash_format[0][:due_date_sort].length
+
+  end
+
+  def test_filter_filter_assignment_data_01
+    file_name = "assignment_data_01"
+    file_as_string = IO.read("#{@@testFileDir}/todolms/ctools/#{file_name}.json")
+
+    file_data = JSON.parse(file_as_string)
+
+    assert_equal 5, file_data.length, 'got multiple assignments'
+
+    filtered = file_data.select { |a| CToolsDirectResponse.filter(a) }
+    logger.debug "#{__method__}: #{__LINE__}: filtered: "+filtered.inspect
+
+    assert_equal 3, filtered.length, 'filter out uninteresting assignments'
+
+  end
+
+
+  def test_filter_filter_assignment_data_no_match
+    file_name = "assignment_data_no_match"
+    file_as_string = IO.read("#{@@testFileDir}/todolms/ctools/#{file_name}.json")
+
+    file_data = JSON.parse(file_as_string)
+
+    assert_equal 5, file_data.length, 'got multiple assignments'
+
+    filtered = file_data.select { |a| CToolsDirectResponse.filter(a) }
+    logger.debug "#{__method__}: #{__LINE__}: filtered: "+filtered.inspect
+
+    assert_equal 0, filtered.length, 'filter out uninteresting assignments'
+
+  end
+
+  def test_filter_filter_assignment_multiple_keys
+    file_name = "assignment_multiple_keys"
+    file_as_string = IO.read("#{@@testFileDir}/todolms/ctools/#{file_name}.json")
+
+    file_data = JSON.parse(file_as_string)
+    #puts "file_data: #{file_data.inspect}"
+    file_data = file_data['Result']['dash_collection']
+    #puts "file_data: result #{file_data.inspect}"
+
+    assert_equal 16, file_data.length, 'got multiple assignments'
+
+    filtered = file_data.select { |a| CToolsDirectResponse.filter(a) }
+
+    assert_equal 13, filtered.length, 'filter out assignments not labeled with due date'
+
+  end
+
+  def test_filter_filter_assignment_null
+
+    empty_assignment = '[{}]'
+
+    list_data = JSON.parse(empty_assignment)
+
+    assert_equal 1, list_data.length, 'got an assignment'
+
+    filtered = list_data.select { |a| CToolsDirectResponse.filter(a) }
+    logger.debug "#{__method__}: #{__LINE__}: filtered: "+filtered.inspect
+
+    assert_equal 0, filtered.length, 'filter out uninteresting assignments'
+
   end
 
 end
